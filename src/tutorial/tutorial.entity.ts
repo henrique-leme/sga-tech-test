8c783d704749d256778a3c241bfa223b069b4b76
HenriqueLeme <henriqueleme.brito@outlook.com>
2024-08-30 19:30:56 -0300

    feat: tutorial entity

---
+++ src/tutorial/tutorial.entity.ts
@@ -0,0 +1,25 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Tutorial {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  title: string;
+
+  @Column()
+  content: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
