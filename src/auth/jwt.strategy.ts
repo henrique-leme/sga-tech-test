ae1dcb1bee6510d9554e690f41df7d66e73bd0b0
HenriqueLeme <henriqueleme.brito@outlook.com>
2024-08-30 19:33:56 -0300

    feat: jwt strategy

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,25 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { AuthService } from './auth.service';
+import { JwtPayload } from './jwt-payload.interface';
+import { User } from '../user/user.entity';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly authService: AuthService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_SECRET || 'your_jwt_secret',
+    });
+  }
+
+  async validate(payload: JwtPayload): Promise<User> {
+    const user = await this.authService.validateToken(payload.sub);
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    return user;
+  }
+}
