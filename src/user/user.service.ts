6c20d1d25cc8d6c46188de595de2c66bec94086a
HenriqueLeme <henriqueleme.brito@outlook.com>
2024-08-30 19:31:45 -0300

    feat: user service

---
+++ src/user/user.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import * as bcrypt from 'bcrypt';
+import { CreateUserDto } from './dto/create-user.dto';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const { name, email, password } = createUserDto;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = this.userRepository.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    return this.userRepository.save(user);
+  }
+
+  async validateUser(email: string, password: string): Promise<User | null> {
+    const user = await this.userRepository.findOne({ where: { email } });
+    if (user && (await bcrypt.compare(password, user.password))) {
+      return user;
+    }
+    return null;
+  }
+
+  async findOne(id: number): Promise<User> {
+    return this.userRepository.findOne({ where: { id } });
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    return this.userRepository.findOne({ where: { email } });
+  }
+}
