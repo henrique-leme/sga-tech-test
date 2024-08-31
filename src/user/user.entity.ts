1bb8dfbac76a91ed10478aaa28b3055fc9868ff4
HenriqueLeme <henriqueleme.brito@outlook.com>
2024-08-31 20:30:48 -0300

    feat: user entity update

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,16 +1,24 @@
 import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { ObjectType, Field, Int } from '@nestjs/graphql';
 
+@ObjectType({ description: 'Entidade que representa um usuário no sistema' })
 @Entity()
 export class User {
+  @Field(() => Int, { description: 'ID único do usuário' })
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Field({
+    description: 'Endereço de e-mail do usuário. Deve ser único no sistema',
+  })
   @Column({ unique: true })
   email: string;
 
+  @Field({ description: 'Senha do usuário armazenada de forma segura' })
   @Column()
   password: string;
 
+  @Field({ description: 'Nome completo do usuário' })
   @Column()
   name: string;
 }
